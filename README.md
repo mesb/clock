886b251a1d524a4dcc893ffc186175de2ee16bc3
mesb <eng.mesembe@gmail.com>
2015-07-06 11:00:31 -0400

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # clock
 A simple clock application using go concurrency patterns
+
+# Concurrency
+"Concurrency is a composition of independently executing things"
+                 -- Rob Pike, Concurrency is not parallism
+
+# About this Application
+This application is a composition of independently executing processes that
+collectively simulate a toy clock. Its purpose is
+investigate and enable me understand go's concurrency primitives and patterns.
+
+# Technique
+## Recursion
+Recursion, in this application, is used to describe processes that run forever.
+
+tick(c chan string)
+updateCount(tick chan string, ctr *Counter, ctrChan chan Counter)
+
+2 recursive functions, running forever. The tick function is the actual "tick"
+of the clock. It constantly ticks and sends the tick to the updateCount function
+that concurrently does the necessary clock arithmetic.
+
+## Channels
+Channels are used as pipelines for sending and receiving messages. Go provides
+channels.
+
+There is one channel that sends ticks from the tick function to the
+updateCounter function.
+
+The updateCounter also has a Counter channel that is used to send updated
+counters to the printer function, which then prints the clock
+
+## Functions
+Functions describe the processes that collectively and concurrently implement
+the toy clock.
+
+There are 3 main processes in this application.
+
+1. tick(c chan string):
+tick describes the ticking of a clock
+
+2. updateCounter(tick chan string, ctr *Counter, ctrChan chan Counter):
+receives a tick, a pointer to the instantiated counter object, and a channel of
+type Counter. It increments seconds when it receives ticks.
+
+## Data Structures
+Counter is the main data structure. It represents the hour, minutes, and
+seconds of a clock.
+
+Also, it has methods that increment seconds, minutes, and hours.
+
+
+# References
+Communicating Sequential Processes, C.A.R Hoare
+Concurrency is Not Parallelism, Rob Pike
--- con.go
+++ con.go
@@ -1,10 +1,20 @@
 package main
 
+/*
+  An implementation of a toy clock using recursion, and go concurrency patterns
+
+*/
+
+
 import (
 	"fmt"
 	"time"
 )
 
+/*
+  tick: Actual ticking of a clock. It sends its ticks via a channel to the
+  counter process, that does clock arithmetic.
+*/
 
 func tick(c chan string) {
 	time.Sleep(time.Second * 1)
@@ -12,6 +22,10 @@ func tick(c chan string) {
 	tick(c)
 }
 
+/*
+  updateCount: receives a tick, updates the seconds of the counter object, and
+  send the counter via a channel tot he printer.
+*/
 func updateCount(tick chan string, ctr *Counter, ctrChan chan Counter) {
 	t := <-tick
 	if t != "" {
@@ -22,12 +36,16 @@ func updateCount(tick chan string, ctr *Counter, ctrChan chan Counter) {
 }
 
 
-
+/*
+  Counter: represents the hour, minute, and seconds of a clock
+*/
 type Counter struct {
 	hh, mm, ss int
 }
 
-
+/*
+ plusSec: Increments seconds of a counter object
+*/
 func (c *Counter) plusSec() {
 	if c.ss == 59 {
 		c.plusMin()
@@ -36,6 +54,9 @@ func (c *Counter) plusSec() {
 	c.ss++
 }
 
+/*
+  plusMin: increments the minutes of a counter object
+*/
 func (c *Counter) plusMin() {
 	if c.mm == 59 {
 		c.PlusHr()
@@ -44,22 +65,40 @@ func (c *Counter) plusMin() {
 	c.mm++
 }
 
+/*
+  plusHr: Incremenets the hours of a counter object
+*/
 func (c *Counter) PlusHr() {
 	if c.hh == 23 {
 		c.hh = 0
 	}
 }
 
+/*
+  String representation of a counter, which is eventually a clock display
+*/
 func (c Counter) String() string{
 	return fmt.Sprintf("%.2d : %.2d : %.2d", c.hh, c.mm, c.ss)
 }
 
+/*
+   printer: receives a counter object via a channel and displays it, thereby
+   showing the clock/counter
+*/
 func printer(C chan Counter) {
 	c := <-C
 	fmt.Printf("%s \r", c)
 	printer(C)
 }
 
+/*
+  main():
+  1. Create a counter object
+  2. create a channel of type string, for sending and receiving ticks
+  3. Create a Counter channel for sending and receiving counter objects
+  4. Concurrently run counter/clock processes
+  5. Block the main function with an input operation.
+*/
 func main() {
 	ctr := &Counter{0, 0, 0}
 	var c chan string = make(chan string)
